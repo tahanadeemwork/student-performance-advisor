56a1e1de531b0f5e4d94144b8a0ce7b057375832
Taha Nadeem <tahanadeemwork@gmail.com>
2026-05-22 23:37:20 +0500

    Initial project setup - folder structure created

---
+++ main.py
@@ -0,0 +1,9 @@
+# Test file - Student Performance Advisor
+print("Python is working!")
+
+import pandas as pd
+import numpy as np
+print("pandas and numpy are working!")
+
+print("Project setup is complete!")
+print("Ready to build the Student Performance Advisor!")
